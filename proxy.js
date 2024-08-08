c2c01494c55ea0b6afeb502d92fa46dc6d7380b1
Lucho <cheluiviajes@gmail.com>
2024-08-08 15:56:22 -0300

    updated and created vercel.json

--- proxy.js
+++ proxy.js
@@ -3,33 +3,42 @@ const request = require("request");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:3000", // Origin local para desarrollo
+  "https://tu-dominio-vercel.vercel.app" // Agrega aquí tu dominio de Vercel cuando esté desplegado
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: "POST",
+  allowedHeaders: ["Content-Type"],
+}));
+
 app.use(express.json());
 
-const API_URL =
-	"https://api.routingapi.com/rtbs.json?key=bc86197f-e418-4893-9af4-0f625ae361f2"; // 803 - 803/809 ACA - CAMPAIGN
+const API_URL = "https://api.routingapi.com/rtbs.json?key=ab514634-6955-4f94-9e22-5de033366d2f"; 
 
 app.post("/proxy", (req, res) => {
-	const { publisher_id, caller_number } = req.body;
+  const { publisher_id, caller_number, first_name, last_name, address, city, state, zip_code, email } = req.body;
 
-	const url = `${API_URL}&publisher_id=${publisher_id}&caller_number=${caller_number}`;
+  const url = `${API_URL}&publisher_id=${publisher_id}&caller_number=${caller_number}&first_name=${first_name}&last_name=${last_name}&address=${address}&city=${city}&state=${state}&zip_code=${zip_code}&email=${email}`;
 
-	request.post(
-		{
-			url: url,
-			json: true,
-			body: req.body,
-		},
-		(error, response, body) => {
-			if (error) {
-				return res.status(500).send(error);
-			}
-			res.status(response.statusCode).send(body);
-		}
-	);
+  request.post(
+    {
+      url: url,
+      json: true,
+      body: req.body,
+    },
+    (error, response, body) => {
+      if (error) {
+        return res.status(500).send(error);
+      }
+      res.status(response.statusCode).send(body);
+    }
+  );
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-	console.log(`Proxy server running on http://localhost:${PORT}`);
+  console.log(`Proxy server running on port ${PORT}`);
 });
